d764401fbc94ded9ebe6a5b940adba0842cb2970
Luqman X Hakim <t3kaje365@gmail.com>
2022-04-10 12:32:08 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,8 @@ Route::redirect('/', 'login');
 
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/dashboard', DashboardController::class)->name('dashboard');
+    Route::get('/category-events', [CategoryEventController::class, 'index'])->name('categoryEvent.index');
+    Route::post('/category-events', [CategoryEventController::class, 'create'])->name('categoryEvent.create');
     Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
     Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::resource('tour', TourController::class );
