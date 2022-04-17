a16a562dcda3562662fdf121d84923f11d3d2963
Luqman X Hakim <t3kaje365@gmail.com>
2022-04-18 00:37:50 +0700

    update-city

--- database/seeders/CitySeeder.php
+++ database/seeders/CitySeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\City;
+use DateTime;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CitySeeder extends Seeder
 {
@@ -18,5 +20,118 @@ class CitySeeder extends Seeder
         // City::factory()
         // ->count(10)
         // ->create();
+
+        $city = [
+            [
+                'name' => 'Surabaya',
+                'province_id' => '1',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+            [
+                'name' => 'Banda Aceh',
+                'province_id' => '4',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+            [
+                'name' => 'Denpasar',
+                'province_id' => '5',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+            [
+                'name' => 'Serang',
+                'province_id' => '7',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+            [
+                'name' => 'Tangerang',
+                'province_id' => '7',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+            [
+                'name' => 'Bengkulu',
+                'province_id' => '8',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+            [
+                'name' => 'Batu',
+                'province_id' => '1',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+            [
+                'name' => 'Blitar',
+                'province_id' => '1',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+            [
+                'name' => 'Kediri',
+                'province_id' => '1',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+            [
+                'name' => 'Madiun',
+                'province_id' => '1',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+            [
+                'name' => 'Malang',
+                'province_id' => '1',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+            [
+                'name' => 'Mojokerto',
+                'province_id' => '1',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+            [
+                'name' => 'Pasuruan',
+                'province_id' => '1',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+            [
+                'name' => 'Probolinggo',
+                'province_id' => '1',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+            [
+                'name' => 'Surakarta',
+                'province_id' => '2',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+            [
+                'name' => 'Magelang',
+                'province_id' => '2',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+            [
+                'name' => 'Pekalongan',
+                'province_id' => '2',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+            [
+                'name' => 'Semarang',
+                'province_id' => '2',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+        ];
+
+        DB::table('cities')->insert($city);
     }
 }
