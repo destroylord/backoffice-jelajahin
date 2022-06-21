2c994cb637c1fb21b255ca4fc6119fd4d5f0613b
destroylord <masapin68@gmail.com>
2022-06-21 09:53:22 +0700

    add table chat messages

---
+++ database/migrations/2022_06_21_095150_create_chat_messages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chat_messages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('message');
+            $table->text('sender_email');
+            $table->dateTime('created_date');
+            $table->string('uuid_chat_room', 36);
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chat_messages');
+    }
+};
